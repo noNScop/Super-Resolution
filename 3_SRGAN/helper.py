8c6b841ae34b0dffa511b9f9fa0d64f634a02580
noNScop <137828231+noNScop@users.noreply.github.com>
2025-04-14 10:23:24 +0200

    Adjusted EDSR notebooks to SRResNet

--- 3_SRGAN/helper.py
+++ 3_SRGAN/helper.py
@@ -24,58 +24,64 @@ transform = v2.Compose([
 ])
 
 class ResBlock(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.block = nn.Sequential(
-            nn.Conv2d(256, 256, 3, stride=1, padding='same'),
-            nn.PReLU(),
-            nn.Conv2d(256, 256, 3, stride=1, padding='same'),
-        )
+        def __init__(self):
+            super().__init__()
+            self.block = nn.Sequential(
+                nn.Conv2d(64, 64, 3, stride=1, padding='same'),
+                nn.BatchNorm2d(64),
+                nn.PReLU(),
+                nn.Conv2d(64, 64, 3, stride=1, padding='same'),
+                nn.BatchNorm2d(64)
+            )
 
-    def forward(self, x):
-        return x + 0.1*self.block(x)
+        def forward(self, x):
+            return x + self.block(x)
 
-class EDSR(nn.Module):
+class SRResNet(nn.Module):
     def __init__(self, n: int):
         """
         Args:
             n: scaling factor
         """
         super().__init__()
-        self.DIV2K_RGB = torch.tensor([0.44882884613943946, 0.43713809810624193, 0.4040371984052683]).to(device)
         
         self.expand = nn.Sequential(
-            nn.Conv2d(3, 256, 9, stride=1, padding='same'),
+            nn.Conv2d(3, 64, 9, stride=1, padding='same'),
             nn.PReLU()
         )
 
         self.residual_blocks = nn.Sequential()
-        for _ in range(32):
+        for _ in range(16):
             self.residual_blocks.append(ResBlock())
 
-        self.residual_blocks.append(nn.Conv2d(256, 256, 3, stride=1, padding='same'))
+        self.residual_blocks.append(nn.Conv2d(64, 64, 3, stride=1, padding='same'))
+        self.residual_blocks.append(nn.BatchNorm2d(64))
 
         self.upscaling_head = nn.Sequential()
         for _ in range(int(math.log2(n))):
-            self.upscaling_head.append(nn.Conv2d(256, 4*256, 3, stride=1, padding='same'))
+            self.upscaling_head.append(nn.Conv2d(64, 256, 3, stride=1, padding='same'))
             self.upscaling_head.append(nn.PixelShuffle(2))
             self.upscaling_head.append(nn.PReLU())
             
-        self.upscaling_head.append(nn.Conv2d(256, 3, 9, stride=1, padding='same'))
+        self.upscaling_head.append(nn.Conv2d(64, 3, 9, stride=1, padding='same'))
 
     def forward(self, x):
-        x = self.expand(x-self.DIV2K_RGB[:, None, None])
-        return self.upscaling_head(self.residual_blocks(x) + x) + self.DIV2K_RGB[:, None, None]
-    
-class EDSR_Dataset(Dataset):
+        x = self.expand(x)
+        return self.upscaling_head(self.residual_blocks(x) + x)
+        
+class SRResNet_Dataset(Dataset):
     def __init__(self, target_paths: list[str], scale: int, ram_limit_gb: float = 2.0):
         self.crop_size = scale * 48
         self.scale = scale
 
-        self.rotations = [0, 90, 180, 270]
-        self.transforms = v2.Compose([
+        self.input_transform = v2.Compose([
             v2.PILToTensor(),
-            v2.Lambda(lambda x: (x / 255.0))
+            v2.Lambda(lambda x: x / 255.0)
+        ])
+        
+        self.target_transform = v2.Compose([
+            v2.PILToTensor(),
+            v2.Lambda(lambda x: (2.0*x / 255.0) - 1)
         ])
 
         self.preloaded = {}
@@ -102,16 +108,8 @@ class EDSR_Dataset(Dataset):
 
         target = self.random_crop(target, self.crop_size)
         inp = target.resize((target.width // self.scale, target.height // self.scale), Image.BICUBIC)
-        
-        rotation =  random.choice(self.rotations)
-        if rotation != 0:
-            inp = v2.functional.rotate(inp, rotation)
-            target = v2.functional.rotate(target, rotation)
-        if random.randint(0, 1):
-            inp = v2.functional.horizontal_flip(inp)
-            target = v2.functional.horizontal_flip(target)
             
-        return self.transforms(inp), self.transforms(target)
+        return self.input_transform(inp), self.target_transform(target)
 
     def random_crop(self, img, size):
         w, h = img.size
@@ -126,16 +124,11 @@ class EDSR_Dataset(Dataset):
 
     def set_crop_size(self, crop_size: int):
         self.crop_size = crop_size
-
+        
 def calc_metrics(model: nn.Module, target_ds: list[str], scale: int):
-    transform_target = v2.Compose([
+    transform = v2.Compose([
         v2.PILToTensor(),
-        v2.Lambda(lambda x: x/255.0)
-    ])
-
-    transform_input = v2.Compose([
-        v2.PILToTensor(),
-        v2.Lambda(lambda x: (x / 255.0))
+        v2.Lambda(lambda x: x / 255.0)
     ])
 
     psnr_acc = 0
@@ -152,11 +145,12 @@ def calc_metrics(model: nn.Module, target_ds: list[str], scale: int):
         target_image = target_image.crop((0, 0, w, h))
         
         lowres = target_image.resize((w // scale, h // scale), resample=Image.BICUBIC)
-        input_tensor = transform_input(lowres).unsqueeze(0).to(device)
-        target_tensor = transform_target(target_image).unsqueeze(0).to(device)
+        input_tensor = transform(lowres).unsqueeze(0).to(device)
+        target_tensor = transform(target_image).unsqueeze(0).to(device)
 
         with torch.inference_mode():
-            sr = model(input_tensor).clamp(0, 1)
+            # convert value range [-1,1] -> [0,1]
+            sr = ((model(input_tensor)+1.0)/2.0).clamp(0.0, 1.0)
 
         psnr_acc += psnr(sr, target_tensor).item()
         ssim_acc += ssim(sr, target_tensor).item()
@@ -176,7 +170,7 @@ def calc_metrics(model: nn.Module, target_ds: list[str], scale: int):
     psnr_acc /= len(target_ds)
     ssim_acc /= len(target_ds)
     return psnr_acc, ssim_acc, lpips_acc
-
+    
 def train_step(model, dataloader, optimizer, loss_fn):
     avg_psnr = 0
     avg_ssim = 0
@@ -192,8 +186,9 @@ def train_step(model, dataloader, optimizer, loss_fn):
         optimizer.step()
         optimizer.zero_grad(set_to_none=True)
 
-        logits = logits.clamp(0.0, 1.0)
-        target = target.clamp(0.0, 1.0)
+        # convert value range [-1,1] -> [0,1]
+        logits = ((logits+1.0)/2.0).clamp(0.0, 1.0)
+        target = ((target+1.0)/2.0).clamp(0.0, 1.0)
         
         avg_psnr += psnr(logits, target).item()
         avg_ssim += ssim(logits, target).item()
@@ -211,9 +206,10 @@ def valid_step(model, dataloader, loss_fn):
     with torch.inference_mode():
         for batch, target in dataloader:
             batch, target = batch.to(device), target.to(device)
-            
-            logits = logits.clamp(0.0, 1.0)
-            target = target.clamp(0.0, 1.0)
+
+            # convert [-1,1] -> [0,1]
+            logits = ((model(batch)+1.0)/2.0).clamp(0.0, 1.0)
+            target = ((target+1.0)/2.0).clamp(0.0, 1.0)
             
             avg_psnr += psnr(logits, target).item()
             avg_ssim += ssim(logits, target).item()
@@ -222,17 +218,15 @@ def valid_step(model, dataloader, loss_fn):
     avg_psnr /= len(dataloader)
     avg_ssim /= len(dataloader)
     avg_lpips /= len(dataloader)
-
-        
+ 
     return avg_psnr, avg_ssim, avg_lpips
 
-def train(model, train_dl, valid_dl, optimizer, scheduler: StepLR, loss_fn, epochs, start_checkpoint=None):
-    os.makedirs('./tmp_model_checkpoints', exist_ok=True)
+def train(model, train_dl, valid_dl, optimizer, loss_fn, epochs, start_checkpoint=None):
+    os.makedirs('../tmp_model_checkpoints', exist_ok=True)
     counter = 0 # count epochs without printing training stats
-    log_freq = epochs // 100 # how often to print stats when no progress is made
     
     if start_checkpoint:
-        start_epoch = start_checkpoint['epoch']
+        start_epoch = start_checkpoint['epoch'] + 1
         best_psnr = start_checkpoint['best_psnr']
         best_ssim = start_checkpoint['best_ssim']
         best_lpips = start_checkpoint['best_lpips']
@@ -242,6 +236,8 @@ def train(model, train_dl, valid_dl, optimizer, scheduler: StepLR, loss_fn, epoc
         best_ssim = 0
         best_lpips = float('inf')
         
+    log_freq = (epochs - start_epoch) // 100 # how often to print stats when no progress is made
+    
     for epoch in tqdm(range(start_epoch, epochs), desc="Epochs"):
         counter += 1
         train_psnr, train_ssim = train_step(
@@ -257,8 +253,6 @@ def train(model, train_dl, valid_dl, optimizer, scheduler: StepLR, loss_fn, epoc
             loss_fn,
         )
 
-        scheduler.step()
-
         progress = False
         
         if valid_psnr > best_psnr:
@@ -269,11 +263,10 @@ def train(model, train_dl, valid_dl, optimizer, scheduler: StepLR, loss_fn, epoc
                 'best_psnr': best_psnr,
                 'best_ssim': best_ssim,
                 'best_lpips': best_lpips,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict()
+                'model_state_dict':  model._orig_mod.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()
             }
-            torch.save(checkpoint, f'./tmp_model_checkpoints/best_psnr.pth')
+            torch.save(checkpoint, f'../tmp_model_checkpoints/best_psnr.pth')
 
         if valid_ssim > best_ssim:
             progress = True
@@ -283,11 +276,10 @@ def train(model, train_dl, valid_dl, optimizer, scheduler: StepLR, loss_fn, epoc
                 'best_psnr': best_psnr,
                 'best_ssim': best_ssim,
                 'best_lpips': best_lpips,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict()
+                'model_state_dict':  model._orig_mod.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()
             }
-            torch.save(checkpoint, f'./tmp_model_checkpoints/best_ssim.pth')
+            torch.save(checkpoint, f'../tmp_model_checkpoints/best_ssim.pth')
 
         if valid_lpips < best_lpips:
             progress = True
@@ -297,11 +289,10 @@ def train(model, train_dl, valid_dl, optimizer, scheduler: StepLR, loss_fn, epoc
                 'best_psnr': best_psnr,
                 'best_ssim': best_ssim,
                 'best_lpips': best_lpips,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict()
+                'model_state_dict':  model._orig_mod.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()
             }
-            torch.save(checkpoint, f'./tmp_model_checkpoints/best_lpips.pth')
+            torch.save(checkpoint, f'../tmp_model_checkpoints/best_lpips.pth')
 
         if epoch == epochs-1:
             checkpoint = {
@@ -309,46 +300,18 @@ def train(model, train_dl, valid_dl, optimizer, scheduler: StepLR, loss_fn, epoc
                 'best_psnr': best_psnr,
                 'best_ssim': best_ssim,
                 'best_lpips': best_lpips,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-                'scheduler_state_dict': scheduler.state_dict()
+                'model_state_dict':  model._orig_mod.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()
             }
-            torch.save(checkpoint, f'./tmp_model_checkpoints/last.pth')
+            torch.save(checkpoint, f'../tmp_model_checkpoints/last.pth')
             
         if progress or counter >= log_freq:
             counter = 0
             print(
                 f"Epoch: {epoch+1} | "
-                f"learning rate: {scheduler.get_last_lr()[0]:.6f} | "
                 f"[train] PSNR: {train_psnr:.4f} | "
                 f"[train] SSIM: {train_ssim:.4f} | "
                 f"[valid] PSNR: {valid_psnr:.4f} | "
                 f"[valid] SSIM: {valid_ssim:.4f} | "
                 f"[valid] LPIPS: {valid_lpips:.4f}"
             )
-
-class GSE:
-    def __init__(self, model):
-        self.model = model
-        self.rotations = [0, 90, 180, 270]
-
-        self.transforms = v2.Compose([
-            v2.PILToTensor(),
-            v2.Lambda(lambda x: x / 255.0)
-        ])
-
-    def __call__(self, x):
-        prediction = torch.zeros(self.model(x).shape).to(device)
-        for rotation in self.rotations:
-            rot = torch.rot90(x, k=rotation // 90, dims=[-2, -1])
-            for i in range(2):
-                with torch.inference_mode():
-                    if i:
-                        flip = v2.functional.horizontal_flip(rot)
-                        out = torch.flip(self.model(flip), dims=[-1])
-                    else:
-                        out = self.model(rot)
-
-                prediction += torch.rot90(out, k=4 - rotation // 90, dims=[-2, -1])
-
-        return prediction / 8.0
